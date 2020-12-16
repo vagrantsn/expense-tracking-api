b12c4ece1fb2e5e88698e94a7f5a55f7a2ec46d0
Vagner S <vagnervst17@gmail.com>
2020-12-16 01:11:48 -0300

    database: refactor uri config for development environment

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,7 +1,4 @@
 module.exports = {
   host: process.env.MONGO_HOST,
   port: process.env.MONGO_PORT,
-  db: process.env.MONGO_DB,
-  user: process.env.MONGO_USER,
-  password: process.env.MONGO_PASSWORD,
 }
--- src/database/mongo.js
+++ src/database/mongo.js
@@ -2,16 +2,9 @@ const mongoose = require('mongoose')
 
 const config = require('../config/mongo.js')
 
-const uri = `mongodb://${config.user}:${config.password}@${config.host}:${config.port}`
+const uri = `mongodb://${config.host}:${config.port}`
 
-const openConnection = async () => mongoose.connect(uri, {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-
-const closeConnection = async () => mongoose.connection.close()
-
-module.exports = {
-  closeConnection,
-  openConnection,
-}
