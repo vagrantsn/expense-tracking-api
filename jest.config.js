9abbd1dcfb48aa31fe71832678d9318021b81e18
Vagner S <vagnervst17@gmail.com>
2020-12-22 00:30:21 -0300

    tests: add jest normal timers on e2e env

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,16 @@
+const testEnv = process.env.TEST_ENV
+
 const config = {
   clearMocks: true,
   coverageProvider: "v8",
   testEnvironment: "node",
-  timers: 'modern',
   testTimeout: 10000,
 }
 
-const testEnv = process.env.TEST_ENV
+if (testEnv === 'unit') {
+  config.timers = 'modern'
+}
+
 if (testEnv === 'e2e') {
   config.testMatch = [
     '**/?(*.)+(e2e).[tj]s?(x)'
