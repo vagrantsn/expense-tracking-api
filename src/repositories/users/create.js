9c1f42a5ee01883a4045e8fcc456f7ad7030cf0f
Vagner S <vagnervst17@gmail.com>
2020-12-26 13:11:38 -0300

    repositories(users) add tests to create function

--- src/repositories/users/create.js
+++ src/repositories/users/create.js
@@ -3,7 +3,11 @@ const db = require('../../database')
 const create = async ({ email, password }) => {
   const user = await new db.User({ email, password }).save()
 
-  return user
+  return {
+    id: user._id.toString(),
+    email: user.email,
+    password: user.password,
+  }
 }
 
 module.exports = create
