5e6dbde6173d42f822e93a514ddff3d87bdb8062
Vagner S <vagnervst17@gmail.com>
2020-12-26 13:15:09 -0300

    repositories(users) add tests to findByEmail function

--- src/repositories/users/findByEmail.js
+++ src/repositories/users/findByEmail.js
@@ -1,8 +1,13 @@
 const db = require('../../database')
 
 const findByEmail = async (email) => {
-  const user = await db.User.where({ email }).findOne()
-  return user
+  const user = await db.User.where({ email }).findOne().lean()
+
+  return {
+    id: user._id.toString(),
+    email: user.email,
+    password: user.password,
+  }
 }
 
 module.exports = findByEmail
