cf264ea793c10108546c5c861d1ea4a89133e434
Vagner S <vagnervst17@gmail.com>
2020-12-26 13:19:40 -0300

    repositories(users) add tests to findById function

--- src/repositories/users/findById.js
+++ src/repositories/users/findById.js
@@ -1,8 +1,12 @@
 const db = require('../../database')
 
 const findById = async (id) => {
-  const user = await db.User.findById(id)
-  return user
+  const user = await db.User.findById(id).lean()
+  return {
+    id: user._id.toString(),
+    email: user.email,
+    password: user.password,
+  }
 }
 
 module.exports = findById
