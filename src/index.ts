98be31baaf66654d0d7e46c3f685cb363d08aad4
Vagner S <vagners.tambalo@gmail.com>
2021-01-21 11:32:02 -0300

    refactor: add import statements to main file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-require('./database/mongo')
+import './database/mongo'
 
-const app = require('./app')
+import app from './app'
 
 const port = process.env.PORT || 3000
 const server = app.listen(port, () => console.log(`Listening on port ${port}`))
